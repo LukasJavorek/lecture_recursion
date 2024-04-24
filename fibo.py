f844bdd78b32f8848f96ff19b6d4ffc3594edd46
LukasJavorek <l.j.brno@seznam.cz>
2024-04-24 10:10:46 +0200

    added fibo1

--- fibo.py
+++ fibo.py
@@ -1,8 +1,10 @@
 
 
 def recursive_nth_fibo(n):
-    if n <= 1:
-        return n
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0
     else:
         return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
